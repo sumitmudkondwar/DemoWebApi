a3634fa7c4f8c5d57a6716277450b9687ce0903a
sumit m <box.sumit@gmail.com>
2024-04-02 17:35:15 +0530

    Authority Added

---
+++ DemoWebApi/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoWebApi.Authority
+{
+    public static class AppRepository
+    {
+        private static List<Application> _applications = new List<Application>()
+        {
+            new Application()
+            {
+                ApplicationId = 1,
+                ApplicationName = "Test",
+                ClientId = "61333E0C-9E2F-4120-9EF6-DCF12706D37B",
+                Secret = "1AB98808-BCE2-42CC-9C58-428AE821DB21"
+            }
+        };
+
+        public static bool Authenticate(string clientId, string secret)
+        { 
+            return _applications.Any(x => x.ClientId == clientId && x.Secret == secret);
+        }
+
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
